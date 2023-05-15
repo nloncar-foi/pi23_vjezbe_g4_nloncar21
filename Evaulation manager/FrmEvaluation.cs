8db238631ad84e58b8c66e96174b4c9bccd40db4
Nino Lončar <nloncar21@foi.hr>
2023-05-15 17:24:44 +0200

    Forma za evaluacije povezana s repozitorijem evaluacije

--- Evaulation manager/FrmEvaluation.cs
+++ Evaulation manager/FrmEvaluation.cs
@@ -15,6 +15,9 @@ namespace Evaulation_manager
     public partial class FrmEvaluation : Form
     {
         private Student student;
+
+        public Student SelectedStudent { get => student; set=> student=value; }
+
         public FrmEvaluation(Student selectedStudent)
         {
             InitializeComponent();
@@ -41,8 +44,26 @@ namespace Evaulation_manager
             currentActivity.MaxPoints;
             txtMinForSignature.Text = currentActivity.MinPointsForSignature + "/" +
             currentActivity.MaxPoints;
+
             numPoints.Minimum = 0;
             numPoints.Maximum = currentActivity.MaxPoints;
+
+            var evaluation = EvaluationRepository.GetEvaluation(SelectedStudent, currentActivity);
+            
+            if(evaluation != null)
+            {
+                txtTeacher.Text = evaluation.Evaluator.ToString();
+
+                txtEvaulationDate.Text = evaluation.EvaluationDate.ToString();
+
+                numPoints.Value = evaluation.Points;
+            }
+            else
+            {
+                txtTeacher.Text=FrmLogin.LoggedTeacher.ToString();
+                txtEvaulationDate.Text = "-";
+                numPoints.Value = 0;
+            }
         }
 
         private void btnCancel_Click(object sender, EventArgs e)
